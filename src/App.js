b85f880c1be174d4299904d9c3316fc087cf5861
midlynj <jmidlyn@gmail.com>
2023-09-22 15:36:52 -0500

    project initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import SignIn from "./component/SignIn";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <nav className="text-amber-300 space-x-28 p-4 bg-black">
+            <Link className="hover:text-violet-400" to={"/"}>
+              Home
+            </Link>
+
+            <Link className="hover:text-violet-400" to={"/signin"}>
+              Sign In
+            </Link>
+
+            <Link className="hover:text-violet-400" to={"/"}>
+              Sign Up
+            </Link>
+
+          </nav>
+
+          <div>
+            <Routes>
+              {/*<Route path="/" element={Home/>} />*/}
+              <Route path="/signin" element={<SignIn/> } />
+              {/*<Route path="/" element={Home/>} />*/}
+              {/*<Route path="/" element={Home/>} />*/}
+
+            </Routes>
+
+          </div>
+
+        </div>
+
+
+      </Router>
     </div>
   );
 }
