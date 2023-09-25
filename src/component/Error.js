591b8b014a396c9b77c2e41b3be60e6c48ef5c23
midlynj <jmidlyn@gmail.com>
2023-09-25 15:48:34 -0500

    error component created

---
+++ src/component/Error.js
@@ -0,0 +1,12 @@
+import "./error.css"
+const Error = () => {
+
+    return (
+        <div className="not-found">
+            <h1 id="error">404</h1>
+            <p>Page Not Found</p>
+        </div>
+    )
+
+}
+export default Error;
