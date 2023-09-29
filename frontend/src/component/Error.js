eeb4fc1f4e3db8ced5f56b48792186a5f1e378e5
midlynj <jmidlyn@gmail.com>
2023-09-28 20:42:52 -0500

    updated class name

--- frontend/src/component/Error.js
+++ frontend/src/component/Error.js
@@ -4,7 +4,7 @@ const Error = () => {
     return (
         <div className="not-found">
             <h1 id="error">404</h1>
-            <p>Page Not Found</p>
+            <p className="gradient">Page Not Found</p>
         </div>
     )
 
