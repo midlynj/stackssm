4e3022c99c2b3afd11964d4574d2d5dd537e4ecb
midlynj <jmidlyn@gmail.com>
2023-10-01 15:59:23 -0500

    created endpoints

--- stacks/src/main/java/com/example/stacks/controller/UserController.java
+++ stacks/src/main/java/com/example/stacks/controller/UserController.java
@@ -1,9 +1,36 @@
 package com.example.stacks.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.stacks.entity.User;
+import com.example.stacks.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping(value = "/api/users", headers = "Accept=application/json")
 public class UserController {
+    private UserService userService;
+
+    @GetMapping("/all")
+    public List<User> fetchAllUsers() {
+        return userService.fetchAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User fetchUserById(@PathVariable Long id) {
+        return userService.fetchUserById(id);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createUser(@RequestBody User newUser) {
+        return userService.createNewUser(newUser);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody User user) {
+        return userService.authenticateUser(user);
+    }
 }
