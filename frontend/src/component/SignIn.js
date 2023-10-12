52ed350839912955baf249b4ed96fe741c8b88c5
midlynj <jmidlyn@gmail.com>
2023-10-12 15:57:49 -0500

    updated sign in

--- frontend/src/component/SignIn.js
+++ frontend/src/component/SignIn.js
@@ -1,10 +1,40 @@
 import "./signup.css"
 import {Link} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {signIn} from "../redux/auth";
+import {useState} from "react";
+
 const SignIn = () => {
+    const dispatch = useDispatch();
+
+    const initialUser = {
+        email: "",
+        password: ""
+    }
+    const [userSignIn, setUserSignIn] = useState(initialUser);
+
+    const handleInputChange = event => {
+        const {name, value} = event.target
+        setUserSignIn({...userSignIn, [name]: value})
+    }
+
+    const handleSignIn = async (e) => {
+        e.preventDefault()
+        const {  email, password } = userSignIn;
+
+        dispatch(signIn({ email, password }))
+            .unwrap()
+            .then(() => {
+                // setSuccessful(true);
+            })
+            .catch(() => {
+                // setSuccessful(false);
+            });
+    };
+
 
     return (
         <>
-
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm profile-picture" style={{
                     border: "3px solid #5a03e7"
@@ -23,7 +53,7 @@ const SignIn = () => {
                 </h2>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form className="space-y-6" action="#" method="POST">
+                    <form className="space-y-6">
                         <div>
                             <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900" style={{
                                 color: "whitesmoke"
@@ -36,6 +66,8 @@ const SignIn = () => {
                                     name="email"
                                     type="email"
                                     autoComplete="email"
+                                    value={userSignIn.email}
+                                    onChange={handleInputChange}
                                     required
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
@@ -60,6 +92,8 @@ const SignIn = () => {
                                     id="password"
                                     name="password"
                                     type="password"
+                                    value={userSignIn.password}
+                                    onChange={handleInputChange}
                                     autoComplete="current-password"
                                     required
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -70,6 +104,7 @@ const SignIn = () => {
                         <div>
                             <button
                                 type="submit"
+                                onClick={handleSignIn}
                                 className="flex w-full justify-center rounded-md bg-indigo-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
                                 Sign in
