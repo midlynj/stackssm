45144730e5c4a6962b001693979f23cb87eb8198
midlynj <jmidlyn@gmail.com>
2023-11-02 18:33:38 -0500

    profile page created

---
+++ stacks/src/main/java/com/example/stacks/interfaces/ImageInterface.java
@@ -0,0 +1,15 @@
+package com.example.stacks.interfaces;
+
+import com.example.stacks.entity.Image;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public interface ImageInterface {
+    ResponseEntity<?> uploadImage(MultipartFile file, Image image) throws SQLException, IOException;
+    ResponseEntity<?> displayImage(Long userId) throws SQLException;
+    ResponseEntity<?> updateImage(MultipartFile file, @PathVariable Long userId) throws SQLException, IOException;
+}
