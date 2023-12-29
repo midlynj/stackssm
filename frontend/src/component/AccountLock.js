b5ffb76a972391e3e9e3721d4d4307484a0e5d69
midlynj <jmidlyn@gmail.com>
2023-12-29 13:03:02 -0600

    account lock component created

---
+++ frontend/src/component/AccountLock.js
@@ -0,0 +1,12 @@
+import "./accountlock.css"
+
+const AccountLock = () => {
+
+    return (
+        <div className="locked-message">
+            <h1>Account is locked</h1>
+        </div>
+    );
+
+}
+export default AccountLock;
