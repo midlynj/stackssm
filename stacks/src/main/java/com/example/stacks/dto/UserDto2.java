9bb8f3c943931128422ff1bb617e20c54e50b654
midlynj <jmidlyn@gmail.com>
2024-01-24 15:33:09 -0600

    added role to dto

--- stacks/src/main/java/com/example/stacks/dto/UserDto2.java
+++ stacks/src/main/java/com/example/stacks/dto/UserDto2.java
@@ -1,15 +1,15 @@
 package com.example.stacks.dto;
 
 import com.example.stacks.entity.Post;
+import com.example.stacks.entity.Role;
 import com.example.stacks.entity.User;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
-
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,9 +19,9 @@ public class UserDto2 {
     private String firstName;
     private String lastName;
     private String email;
+    private Set<Role> role;
 
-    @JsonIgnoreProperties({"friends", "friendsList","createdAt", "posts","password"})
-    @ToString.Exclude
+    @JsonIgnoreProperties({"friends", "friendsList","createdAt", "posts","password","status","roles"})
     private List<User> friends;
 
     @JsonIgnoreProperties("author")
