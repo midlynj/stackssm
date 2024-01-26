64a4698c39ccad84b9c1f6b50bc21c49c8f88d9c
midlynj <jmidlyn@gmail.com>
2024-01-26 15:30:09 -0600

    created error component

---
+++ frontend/src/component/Error.js
@@ -0,0 +1,12 @@
+import "./error.css"
+const Error = () => {
+    return (
+        <div className="cyberpunk-error-container">
+            <div className="error-message">
+                <span>SORRY</span>
+                <p>There's a problem on our end in the cybernetic cityscape...</p>
+            </div>
+        </div>
+    );
+}
+export default Error;
