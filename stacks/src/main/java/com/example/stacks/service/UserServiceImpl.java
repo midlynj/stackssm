e6c8ce1e8cbd2726f0ca1174a573f470e5462ea4
midlynj <jmidlyn@gmail.com>
2024-01-30 14:27:00 -0600

    validating new user email format

--- stacks/src/main/java/com/example/stacks/service/UserServiceImpl.java
+++ stacks/src/main/java/com/example/stacks/service/UserServiceImpl.java
@@ -12,6 +12,8 @@ import com.example.stacks.repository.ImageRepository;
 import com.example.stacks.repository.RoleRepository;
 import com.example.stacks.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 @Service
 @AllArgsConstructor
@@ -27,11 +30,18 @@ public class UserServiceImpl implements UserInterface {
     private final ImageRepository imageRepository;
     private final RoleRepository roleRepository;
 
+
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
     public boolean verifyPassword(String rawPassword, String encodedPassword) {
         return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+
+    public static boolean patternMatches(String emailAddress, String regexPattern) {
+        return Pattern.compile(regexPattern)
+                .matcher(emailAddress)
+                .matches();
     }
 
     @Override
@@ -63,11 +73,18 @@ public class UserServiceImpl implements UserInterface {
         Optional<Image> imageOptional = imageRepository.findById(1L);
         Image defaultImage = imageOptional.get();
 
-        if (userOptional.isPresent())
-            return ResponseEntity.badRequest().body(new MessageResponse("Email is already in use"));
+        if (!patternMatches(newUser.getEmail(), "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@" + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")) {
 
-        if (newUser.getPassword().length() <= 2)
+            return ResponseEntity.badRequest().body(new MessageResponse("Invalid email format"));
+        }
+
+        if (userOptional.isPresent()) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Email is already in use"));
+        }
+
+        if (newUser.getPassword().length() <= 2) {
             return ResponseEntity.badRequest().body(new MessageResponse("Password must be minimum 3 characters"));
+        }
 
         String hashedPassword = passwordEncoder.encode(newUser.getPassword());
 
@@ -101,6 +118,8 @@ public class UserServiceImpl implements UserInterface {
         user.setStatus(Status.ACTIVE);
         userRepository.save(user);
 
+
+
         return ResponseEntity.ok(new MessageResponse("New user registered successfully"));
     }
 
@@ -108,18 +127,22 @@ public class UserServiceImpl implements UserInterface {
     public ResponseEntity<?> authenticateUser(SignIn signInCredentials) {
         Optional<User> userOptional = userRepository.findUserByEmail(signInCredentials.getEmail());
 
-        if (userOptional.isEmpty())
+        if (userOptional.isEmpty()) {
             throw new RuntimeException("User does not exist");
+        }
 
         User userExist = userOptional.get();
 
-        if (userExist.getStatus().equals(Status.INACTIVE))
+        if (userExist.getStatus().equals(Status.INACTIVE)) {
             return ResponseEntity.badRequest().body(new MessageResponse("Account is locked"));
+        }
 
-        if (!verifyPassword(signInCredentials.getPassword(), userExist.getPassword()))
+        if (!verifyPassword(signInCredentials.getPassword(), userExist.getPassword())) {
             return ResponseEntity.badRequest().body(new MessageResponse("Bad credentials"));
+        }
 
         UserDto2 userDto = new UserDto2(userExist.getId(), userExist.getFirstName(), userExist.getLastName(), userExist.getEmail(),userExist.getRoles(), userExist.getFriends(), userExist.getPosts());
+
 
         return ResponseEntity.ok(userDto);
     }
