14fba76b24aa2f33ec03e78b9af6c1f80355ba3b
Midlyn Joseph <Midlyn.Joseph@ibm.com>
2024-03-26 12:49:10 -0500

    added user signin int test

--- stacks/src/test/java/com/example/stacks/UserIntegrationTest.java
+++ stacks/src/test/java/com/example/stacks/UserIntegrationTest.java
@@ -1,21 +1,28 @@
 package com.example.stacks;
 
 import com.example.stacks.dto.UserDto;
+import com.example.stacks.dto.UserDto2;
 import com.example.stacks.entity.User;
 import com.example.stacks.payload.MessageResponse;
+import com.example.stacks.payload.SignIn;
 import com.example.stacks.payload.Signup;
+
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.jdbc.Sql;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 //Use @SpringBootTest for tests that cover the whole Spring Boot application from incoming request to database.
 
@@ -26,6 +33,8 @@ public class UserIntegrationTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+    @MockBean
+    BCryptPasswordEncoder passwordEncoder;
 
     @Test
     @Sql("/m.sql")
@@ -89,6 +98,27 @@ public class UserIntegrationTest {
         User user = response.getBody();
         assertEquals("Danie", user.getFirstName());
     }
+
+    @Test
+//    @Sql("/m.sql")
+    public void testSignIn() {
+            SignIn signIn = new SignIn("joe@email.com","123");
+            when(passwordEncoder.matches("123", signIn.getPassword())).thenReturn(true); // Stub password encoder behavior
+
+            HttpEntity<SignIn> requestEntity = new HttpEntity<>(signIn);
+
+            ResponseEntity<UserDto2> response = restTemplate.exchange(
+                    "http://localhost:" + port + "/api/users/signin",
+                    HttpMethod.POST,
+                    requestEntity,
+                    UserDto2.class
+
+                    );
+                     System.out.println(response);
+                     assertEquals("joe@email.com", response.getBody().getEmail());
+                     assertEquals("Joe", response.getBody().getFirstName());
+        }
+
 
 //alternative to testAllUsers method
 //    @Test
